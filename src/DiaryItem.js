755a0995cb91c5e1e2ae3302ef1d25053b0eb0c0
youzyko <gokongjin@naver.com>
2023-02-09 10:20:17 +0900

    CRUD complete

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -11,8 +11,13 @@ const DiaryItem = ({
   //수정 중인지 여부 버튼기능...true=수정취소,완료 버튼 ...false 수정하기,삭제하기 버튼
   const [isEdit, setIsEdit] = useState(false);
 
+  //수정하기버튼을 클릭할시 isEdit=true값으로 변함...수정취소/완료
   const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  //수정취소를 누를때 기존 내용 보여주기
   const [localContent, setLocalContent] = useState(content);
+
+  //초기화 버튼을 클릭했을때 포커스 맞추기/수정완료
   const localContentInput = useRef();
 
   //삭제
@@ -22,9 +27,10 @@ const DiaryItem = ({
     }
   }; //handleRemove end
 
+  //수정상태에서 나갈때 일어나는 함수
   const handleQuitEdit = () => {
     setIsEdit(false); //수정하기,삭제버튼
-    setLocalContent(content);
+    setLocalContent(content); //기존 내용 보이기
   };
 
   const handleEdit = () => {
@@ -38,6 +44,7 @@ const DiaryItem = ({
       toggleIsEdit();
     }
   };
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -46,12 +53,36 @@ const DiaryItem = ({
         </span>
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
-        <div className="content">{content}</div>
+        <div className="content">
+          {isEdit ? (
+            <>
+              <textarea
+                ref={localContentInput}
+                value={localContent}
+                onChange={(e) => setLocalContent(e.target.value)}
+              />
+            </>
+          ) : (
+            <>{content}</>
+          )}
+        </div>
+        {isEdit ? (
+          <>
+            {" "}
+            {/* ture 일때 */}
+            <button onClick={handleQuitEdit}>수정취소</button>
+            <button onClick={handleEdit}>수정완료</button>
+          </>
+        ) : (
+          <>
+            {/*false 일때 */}
+            <button onClick={toggleIsEdit}>수정하기</button>
+            <button onClick={handleRemove}>삭제하기</button>
+          </>
+        )}
       </div>
 
-      <div>
-        <button onClick={handleRemove}>삭제하기</button>
-      </div>
+      <div></div>
     </div>
   );
 };
